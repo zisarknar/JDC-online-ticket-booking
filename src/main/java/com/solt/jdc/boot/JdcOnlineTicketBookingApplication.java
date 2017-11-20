d1f24f64a0861fee5e4baada0ff3f4d144e4439b
Sargon Zi <iamsargonzi@gmail.com>
2017-11-20 15:10:48 +0630

    initial commit

---
+++ src/main/java/com/solt/jdc/boot/JdcOnlineTicketBookingApplication.java
@@ -0,0 +1,12 @@
+package com.solt.jdc.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdcOnlineTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdcOnlineTicketBookingApplication.class, args);
+	}
+}
