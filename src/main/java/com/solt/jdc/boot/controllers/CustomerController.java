6f5f24ac5ea82204e77859ea2571db8c3033819e
Dell\User <zawgyi.gog@gmail.com>
2017-11-23 15:18:31 +0630

    Test Commit

--- src/main/java/com/solt/jdc/boot/controllers/CustomerController.java
+++ src/main/java/com/solt/jdc/boot/controllers/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RestController
+
 public class CustomerController {
 
     private CustomerService customerService;
