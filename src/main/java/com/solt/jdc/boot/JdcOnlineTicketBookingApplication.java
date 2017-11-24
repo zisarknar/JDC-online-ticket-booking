9c468c4b018f5637202d0a456e5027d21f41e614
Sargon Zi <iamsargonzi@gmail.com>
2017-11-24 11:04:55 +0630

    uniform code

--- src/main/java/com/solt/jdc/boot/JdcOnlineTicketBookingApplication.java
+++ src/main/java/com/solt/jdc/boot/JdcOnlineTicketBookingApplication.java
@@ -12,29 +12,22 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import com.solt.jdc.boot.services.CitiesService;
 
 @SpringBootApplication
-public class JdcOnlineTicketBookingApplication  {
+public class JdcOnlineTicketBookingApplication {
 
-	@Autowired
-	CitiesService citiesService;
-	
-	
-	
-	@Bean
-	public LocaleResolver resolver() {
-		SessionLocaleResolver resolver = new SessionLocaleResolver();
-		resolver.setDefaultLocale(new Locale("en"));
-		return resolver;
-	}
-	
-	
-	
+    @Autowired
+    CitiesService citiesService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(JdcOnlineTicketBookingApplication.class, args);
-	}
+    @Bean
+    public LocaleResolver resolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en"));
+        return resolver;
+    }
 
 
+    public static void main(String[] args) {
+        SpringApplication.run(JdcOnlineTicketBookingApplication.class, args);
+    }
 
 
-	
 }
