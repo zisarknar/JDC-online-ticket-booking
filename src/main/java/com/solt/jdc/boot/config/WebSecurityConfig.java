d41b33446fd1325d79f5cead353f085c0f8e654f
Sargon Zi <iamsargonzi@gmail.com>
2017-12-04 11:05:23 +0630

    csrf tocken

--- src/main/java/com/solt/jdc/boot/config/WebSecurityConfig.java
+++ src/main/java/com/solt/jdc/boot/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/admin/**")
                 .authenticated()
                 .and()
