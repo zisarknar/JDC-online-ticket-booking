bfc4f32d6fd25c90c2bcc77fd6004f27d52a1b7d
Sargon Zi <iamsargonzi@gmail.com>
2017-12-04 11:22:37 +0630

    added and remove some code.

--- src/main/java/com/solt/jdc/boot/config/WebSecurityConfig.java
+++ src/main/java/com/solt/jdc/boot/config/WebSecurityConfig.java
@@ -17,28 +17,28 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-	 @Autowired
-	    private AuthenticationSuccessHandler authenticationSuccessHandler;
-	
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
     @Autowired
     private AccessDeniedHandler accessDeniedHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()               
-        		.antMatchers("/admin/roots/**").hasRole("ROOT")
-        		.antMatchers("/customers/**").hasRole("ROOT")
-        		.antMatchers("/customerdetails/**").hasAnyRole("ROOT","CUSTOMER")        	
-        		.antMatchers("/admin/bookings/**").hasAnyRole("ROOT","MANAGER","STAFF")
-        		.antMatchers("/admin/buses/**").hasAnyRole("ROOT","MANAGER")
-        		.antMatchers("/admin/addresses/**").hasAnyRole("ROOT","MANAGER")
-        		.antMatchers("/admin/bustypes/**").hasAnyRole("ROOT","MANAGER")
-        		.antMatchers("/admin/cities/**").hasAnyRole("ROOT","MANAGER")
-        		.antMatchers("/admin/drivers/**").hasAnyRole("ROOT","MANAGER")
-        		.antMatchers("/admin/services/**").hasAnyRole("ROOT","MANAGER")
-        		.antMatchers("/admin/stations/**").hasAnyRole("ROOT","MANAGER")
-        		.antMatchers("/admin/trips/**").hasAnyRole("ROOT","MANAGER")
-        		.antMatchers("/admin/users/**").hasAnyRole("ROOT","MANAGER")        		
+        http.authorizeRequests()
+                .antMatchers("/admin/roots/**").hasRole("ROOT")
+                .antMatchers("/customers/**").hasRole("ROOT")
+                .antMatchers("/customerdetails/**").hasAnyRole("ROOT", "CUSTOMER")
+                .antMatchers("/admin/bookings/**").hasAnyRole("ROOT", "MANAGER", "STAFF")
+                .antMatchers("/admin/buses/**").hasAnyRole("ROOT", "MANAGER")
+                .antMatchers("/admin/addresses/**").hasAnyRole("ROOT", "MANAGER")
+                .antMatchers("/admin/bustypes/**").hasAnyRole("ROOT", "MANAGER")
+                .antMatchers("/admin/cities/**").hasAnyRole("ROOT", "MANAGER")
+                .antMatchers("/admin/drivers/**").hasAnyRole("ROOT", "MANAGER")
+                .antMatchers("/admin/services/**").hasAnyRole("ROOT", "MANAGER")
+                .antMatchers("/admin/stations/**").hasAnyRole("ROOT", "MANAGER")
+                .antMatchers("/admin/trips/**").hasAnyRole("ROOT", "MANAGER")
+                .antMatchers("/admin/users/**").hasAnyRole("ROOT", "MANAGER")
                 .and()
                 .formLogin()
                 .loginPage("/login")
